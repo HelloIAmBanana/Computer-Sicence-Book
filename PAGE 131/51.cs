7ee460fb644222aafc4133c62151cbca40fe0984
Banana <34979048+HelloIAmBanana@users.noreply.github.com>
2020-11-11 10:27:22 +0200

    Create 51.cs

---
+++ PAGE 131/51.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Transactions;
+
+namespace class_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = 0, max = -999999999999999999, min = 9999999999999999999;
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("enter number");
+                num = double.Parse(Console.ReadLine());
+                if (num >= 0)
+                {
+                    if (min > num)
+                        min = num;
+                }
+                else
+                {
+                    if (max < num) 
+                        max = num;
+                }
+                    
+            }
+            Console.WriteLine("the max negetive number was {0} and the lowest positive number was {1}",max,min);
+        }
+    }
+}
